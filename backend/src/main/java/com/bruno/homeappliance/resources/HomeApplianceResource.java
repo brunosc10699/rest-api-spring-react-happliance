49f5f49295ec92b993b241992847943b7ca9dd0c
brunosc10699 <brunosc10699@gmail.com>
2021-09-22 14:45:39 -0300

    changing @RequestParam to @PathVariable

--- backend/src/main/java/com/bruno/homeappliance/resources/HomeApplianceResource.java
+++ backend/src/main/java/com/bruno/homeappliance/resources/HomeApplianceResource.java
@@ -4,10 +4,7 @@ import com.bruno.homeappliance.dto.HomeApplianceDTO;
 import com.bruno.homeappliance.services.impl.HomeApplianceServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/appliances")
@@ -16,8 +13,8 @@ public class HomeApplianceResource {
 
     private final HomeApplianceServiceImpl homeApplianceService;
 
-    @GetMapping(value = "/name")
-    public ResponseEntity<HomeApplianceDTO> findByNameIgnoreCase(@RequestParam String name){
+    @GetMapping(value = "/{name}")
+    public ResponseEntity<HomeApplianceDTO> findByNameIgnoreCase(@PathVariable String name){
         return ResponseEntity.ok(homeApplianceService.findByNameIgnoreCase(name));
     }
 }
